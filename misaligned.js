fe2a3890ffd3c762525cf7636da59a7f31f86924
MilindAdiga <124535083+MilindAdiga@users.noreply.github.com>
2024-08-07 22:41:05 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -17,7 +17,31 @@ function print_color_map() {
 const result = print_color_map();
 expect(result).equals(25);
 
-const expectedOutput = `0 | White | Blue`;
+const expectedOutput = `0 | White | Blue
+1 | White | Orange
+2 | White | Green
+3 | White | Brown
+4 | White | Slate
+5 | Red | Blue
+6 | Red | Orange
+7 | Red | Green
+8 | Red | Brown
+9 | Red | Slate
+10 | Black | Blue
+11 | Black | Orange
+12 | Black | Green
+13 | Black | Brown
+14 | Black | Slate
+15 | Yellow | Blue
+16 | Yellow | Orange
+17 | Yellow | Green
+18 | Yellow | Brown
+19 | Yellow | Slate
+20 | Violet | Blue
+21 | Violet | Orange
+22 | Violet | Green
+23 | Violet | Brown
+24 | Violet | Slate`;
 
 expect(execSync('node misaligned.js').toString().trim()).equals(expectedOutput);
 
